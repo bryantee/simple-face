38cee07bb30eb26c7913109e63ed0ed975222fd0
Bryan Swagerty <bryantee@gmail.com>
2018-02-18 16:23:47 -0700

    Fix bug: PadZero not happening when single digit minutes

--- common/utils.ts
+++ common/utils.ts
@@ -1,7 +1,8 @@
 // Add zero in front of numbers < 10
-export function zeroPad(i: string): number {
+export function zeroPad(i: string): string {
   if (parseInt(i) < 10) {
+    i = i.toString();
     i = `0${i}`;
   }
-  return parseInt(i);
+  return i;
 }
